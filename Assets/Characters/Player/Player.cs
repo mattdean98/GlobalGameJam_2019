f7213744f12fd8a23ad552c5eb64d3a1019da3e4
Matt Dean <mattdean98@gmail.com>
2019-01-26 11:19:12 -0600

    added sprint and tiles folder

--- Assets/Characters/Player/Player.cs
+++ Assets/Characters/Player/Player.cs
@@ -16,6 +16,7 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //walk
         if (Input.GetKeyDown(KeyCode.W))
         {
             _Player.transform.Translate(0f, 1f * WalkingDistance, 0f);
@@ -31,6 +32,24 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.D))
         {
             _Player.transform.Translate(1f * WalkingDistance, 0f, 0f);
+        }
+
+        //run
+        if (Input.GetKeyDown(KeyCode.W) && Input.GetKey(KeyCode.LeftShift))
+        {
+            _Player.transform.Translate(0f, 2f * WalkingDistance, 0f);
+        }
+        if (Input.GetKeyDown(KeyCode.A) && Input.GetKey(KeyCode.LeftShift))
+        {
+            _Player.transform.Translate(-2f * WalkingDistance, 0f, 0f);
+        }
+        if (Input.GetKeyDown(KeyCode.S) && Input.GetKey(KeyCode.LeftShift))
+        {
+            _Player.transform.Translate(0f, -2f * WalkingDistance, 0f);
+        }
+        if (Input.GetKeyDown(KeyCode.D) && Input.GetKey(KeyCode.LeftShift))
+        {
+            _Player.transform.Translate(2f * WalkingDistance, 0f, 0f);
         }
     }
 }
