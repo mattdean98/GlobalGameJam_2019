ac513f4dc4416079be68ee261ad14f865c8b6a38
Matt Dean <mattdean98@gmail.com>
2019-01-26 10:09:50 -0600

    added basic movement

---
+++ Assets/Characters/Player/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int WalkingDistance;
+    public GameObject _Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //WalkingDistance = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            _Player.transform.Translate(0f, 1f * WalkingDistance, 0f);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            _Player.transform.Translate(-1f * WalkingDistance, 0f, 0f);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            _Player.transform.Translate(0f, -1f * WalkingDistance, 0f);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            _Player.transform.Translate(1f * WalkingDistance, 0f, 0f);
+        }
+    }
+}
