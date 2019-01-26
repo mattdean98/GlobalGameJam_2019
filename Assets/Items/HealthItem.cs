090f75ceb8e150e5ec0680f197a8c9e7bf220a2a
Matt Dean <mattdean98@gmail.com>
2019-01-26 11:46:33 -0600

    fixed controls

---
+++ Assets/Items/HealthItem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthItem : MonoBehaviour
+{
+
+    public GameObject Item;
+    public GameObject OtherObject;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("collided with: " + collision.collider.name);
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("left collision!");
+    }
+}
