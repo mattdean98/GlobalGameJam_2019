ca3de0058ad569b5226886928532d7029f25138e
Matt Dean <mattdean98@gmail.com>
2019-01-26 13:32:54 -0600

    added path stuff

---
+++ Assets/Controllers/GuardController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardController : MonoBehaviour
+{
+    public GameObject Guard1;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Guard1Path()
+    {
+        //do stuff
+
+    }
+    
+}
---
+++ Assets/Controllers/PathFollower.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFollower : MonoBehaviour
+{
+    Node[] PathNode;
+
+    public GameObject Guard1;
+    public float MoveSpeed;
+
+    float Timer;
+
+    int CurrentNode;
+    static Vector3 CurrentPositionHolder;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PathNode = GetComponentsInChildren<Node>();
+        CheckNode();
+
+        //foreach(var i in PathNode)
+        //{
+        //    Debug.Log(i.name);
+        //}
+    }
+
+    void CheckNode()
+    {
+        Timer = 0;
+        if (PathNode[CurrentNode] != null)
+        {
+            CurrentPositionHolder = PathNode[CurrentNode].transform.position;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(CurrentNode);
+        Timer += Time.deltaTime * MoveSpeed;
+
+        if(Guard1.transform.position != CurrentPositionHolder)
+        {
+            Guard1.transform.position = Vector3.Lerp(Guard1.transform.position, CurrentPositionHolder, Timer);
+        }
+        else
+        {
+            CurrentNode++;
+            CheckNode();
+        }
+    }
+}
